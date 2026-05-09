70848ce45b74b7fe704c0d62c0fc7e236343ae9a
M8477 <91190037+M8477@users.noreply.github.com>
2026-05-09 13:05:24 +0100

    Update start_zergpool.sh

--- start_zergpool.sh
+++ start_zergpool.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-cpu_threads=$(grep -c '^processor' /proc/cpuinfo)
-echo "Start SRBMiner-MULTI with Parameters: --algorithm $ALGO --pool $POOL_ADDRESS --wallet LTC:$WALLET_USER.$HOSTNAME#Jumper --password $PASSWORD $EXTRAS --cpu-threads $cpu_threads"
-./SRBMiner-MULTI --algorithm $ALGO --pool $POOL_ADDRESS --wallet LTC:$WALLET_USER.$HOSTNAME#Jumper --password $PASSWORD $EXTRAS --cpu-threads $cpu_threads
+
+# Default fallbacks if not provided
+ALGO=${ALGO:-"kheavyhash"}
+POOL_ADDRESS=${POOL_ADDRESS:-"stratum+tcp://heavyhash.eu.mine.zergpool.com:5137"}
+WALLET_USER=${WALLET_USER:-""}
+PASSWORD=${PASSWORD:-"c=BTC"}
+EXTRAS=${EXTRAS:-"--disable-gpu --api-enable --api-port 21550"}
+
+echo "Starting SRBMiner-MULTI v2.9.8..."
+echo "Algorithm: $ALGO"
+echo "Pool: $POOL_ADDRESS"
+echo "Wallet: $WALLET_USER"
+echo "Password: $PASSWORD"
+echo "Extras: $EXTRAS"
+
+./SRBMiner-MULTI --algorithm "$ALGO" --pool "$POOL_ADDRESS" --wallet "$WALLET_USER" --password "$PASSWORD" $EXTRAS
